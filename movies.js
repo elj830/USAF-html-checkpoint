cda3665aaf97cb9c065fbb95d98e42f573cab5f4
Robert Groppetti <robgroppetti@gmail.com>
2020-06-12 10:50:32 -0500

    Added new files

---
+++ movies.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    // var movies = [
+    // {
+    //     src : "https://m.media-amazon.com/images/M/MV5BN2JlZTBhYTEtZDE3OC00NTA3LTk5NTQtNjg5M2RjODllM2M0XkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_.jpg",
+    //     ref : "littleMermaid.html"
+    // },
+    // {
+    //     src : "https://images-na.ssl-images-amazon.com/images/I/81HK7Ku1-SL.jpg",
+    //     ref : "mulan.html"
+    // }
+    // ]
+})
